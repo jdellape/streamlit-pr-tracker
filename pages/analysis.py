96221e7663ed93ea261dd30aa33b8500f8cf34f4
John Dellape <dellapejw@gmail.com>
2022-06-14 16:57:03 -0400

    fix sorting

--- pages/analysis.py
+++ pages/analysis.py
@@ -29,7 +29,7 @@ st.metric(label=last_pr['lift'].iloc[0] + ' (' + str(last_pr['reps'].iloc[0]) + 
 
 #Reformat date and sort it so that it displays most recent prs at top
 df['date'] = pd.to_datetime(df['date']).dt.date
-df.sort('date', ascending=False)
+df = df.sort_values(by=['date'], ascending=False)
 
 chart = alt.Chart(df).mark_line(
     point=alt.OverlayMarkDef(color="red")
